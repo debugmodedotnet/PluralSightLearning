ca0aa1f07e20e7a37b7bc054940ad6bb08b1251f
debugmode@outlook.com <debugmode@outlook.com>
2014-08-18 22:00:43 +0530

    Add Version 1

---
+++ eManager.Web/Controllers/DepartmentController.cs
@@ -0,0 +1,26 @@
+﻿using eManager.Domain;
+using eManager.Web.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eManager.Web.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        //
+        // GET: /Department/
+        IDepartmentDataSource _db = new DepartmentDb(); 
+
+        public ActionResult Details(int id)
+        {
+
+            var result = _db.Departments.Single(x => x.Id == id);
+           return View(result);
+        }
+
+        
+    }
+}
---
+++ eManager.Web/Controllers/EmployeeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using eManager.Domain;
+using eManager.Web.Infrastructure;
+using eManager.Web.Models;
+
+namespace eManager.Web.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        IDepartmentDataSource _db = new DepartmentDb();
+
+        [HttpGet]
+        public ActionResult Add(int departmentId)
+        {
+            var model = new CreateEmployeeViewModel();
+            model.DepartmentId = departmentId;
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Add(CreateEmployeeViewModel viewModel)
+        {
+            if(ModelState.IsValid)
+            {
+                var department = _db.Departments.Single(x => x.Id == viewModel.DepartmentId);
+                var employee = new Employee
+                {
+                    Name = viewModel.Name,
+                    HiredDate = viewModel.HiredDate
+                };
+                department.Employees.Add(employee);
+
+                _db.Save();
+                return RedirectToAction("details", "department", new { id = viewModel.DepartmentId });
+            }
+            else
+            {
+                return View(viewModel);
+            }
+        }
+
+    }
+}
